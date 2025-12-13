ee3b7da4f7d5d1e21e3b875bf04e641f08bdb5b8
Israel de Souza Lima <israel.lima3@fatec.sp.gov.br>
2025-12-13 10:33:57 +0000

    adiciona condicional em migrations para o modo Live Run

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,14 +2,33 @@ import { runner } from 'node-pg-migrate';
 import { join } from 'path';
 
 export default async function migrations(req, res){
-  const migrationsResponse = await runner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join('infra', 'migrations'),
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  })
-  
-  res.status(200).json(migrationsResponse)
+
+  if (req.method === 'GET'){
+    const migrationsResponse = await runner({   
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join('infra', 'migrations'),
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pgmigrations',
+    })
+    
+    return res.status(200).json(migrationsResponse)
+  }
+
+  if (req.method === 'POST'){
+    const migrationsResponse = await runner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join('infra', 'migrations'),
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pgmigrations',
+    })
+    
+    return res.status(200).json(migrationsResponse)
+  }
+
+  return res.status(405).end()
+    
 }
