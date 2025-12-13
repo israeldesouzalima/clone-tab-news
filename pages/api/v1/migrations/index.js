59a63ed80feacef13b2cd92c93c226a67b5d8532
Israel de Souza Lima <israel.lima3@fatec.sp.gov.br>
2025-12-13 19:23:59 +0000

    refactor: nome das variaveis em /migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,30 +3,31 @@ import { join } from 'path';
 
 export default async function migrations(req, res){
 
+  const defaultOptionsMigration = {   
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations'
+  }
+
   if (req.method === 'GET'){
-    const migrationsResponse = await runner({   
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pgmigrations',
-    })
-    
-    return res.status(200).json(migrationsResponse)
+    const pendingMigrations = await runner(defaultOptionsMigration)
+    return res.status(200).json(pendingMigrations)
   }
 
   if (req.method === 'POST'){
-    const migrationsResponse = await runner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pgmigrations',
+    const migrations = await runner({
+      ...defaultOptionsMigration,
+      dryRun: false
     })
+
+    if (migrations.length > 0) {
+      return res.status(201).json(migrations)
+    }
     
-    return res.status(200).json(migrationsResponse)
+    return res.status(200).json(migrations)
   }
 
   return res.status(405).end()
--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -7,12 +7,21 @@ async function resetDatabase() {
 beforeAll(resetDatabase)
 
 test('POST /api/v1/migrations deve retornar 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/migrations', {
+  const response1 = await fetch('http://localhost:3000/api/v1/migrations', {
     method: "POST"
   })
-  expect(response.status).toBe(200)
+  expect(response1.status).toBe(201)
 
-  const responseBody = await response.json()
-  expect(Array.isArray(responseBody)).toBe(true)
+  const response1Body = await response1.json()
+  expect(Array.isArray(response1Body)).toBe(true)
+
+  const response2 = await fetch('http://localhost:3000/api/v1/migrations', {
+    method: "POST"
+  })
+  expect(response2.status).toBe(200)
+
+  const response2Body = await response2.json()
+  expect(Array.isArray(response2Body)).toBe(true)
+  expect(response2Body.length).toBe(0)
 
 })
